62d6697daf3fcd1066a1501b1e90eb88a529bf0a
Marlena Bowen <marlena.bowen@S04391-MBPR.local>
2019-04-02 16:26:11 -0600

    Adds comments to my_app

--- my_app/app.py
+++ my_app/app.py
@@ -8,13 +8,16 @@ def hello():
 
 @app.route("/health")
 def health_check():
+  # curl my_app
   requests.get('http://localhost:3000/')
 
+  # curl my_app_dependency and rescue from error
   try:
     requests.get('http://localhost:5000')
   except Exception:
+    # will execute if my_app_dependency is down
     return "My app is up but my dependency is down!"
-
+  # will execute if my_app_dependency is up
   return "My app is up and so is my dependency!"
 
 if __name__ == "__main__":
